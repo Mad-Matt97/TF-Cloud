33636db448cdbb5b0a13eb0dcd44e02f275a72ee
unknown <toledotoledomiguel@gmail.com>
2023-11-22 09:40:50 +0000

    fix carpeta .terraform v3

--- providers.tf
+++ providers.tf
@@ -16,7 +16,6 @@ terraform {
 provider "aws" {
   # Configuration options
   region = "us-east-1"
-  alias  = "virginia"
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
   default_tags {
