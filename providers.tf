10974aecb4f93fdd0043491253f5ca6b1d92a9a0
unknown <toledotoledomiguel@gmail.com>
2023-11-22 09:22:14 +0000

    fix carpeta .terraform v2

--- providers.tf
+++ providers.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">=5.23.1, <5.23.2"
+      version = ">=5.23.1"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "3.5.1"
+      version = ">=3.5.1"
     }
   }
   required_version = ">1.2.0"
@@ -17,10 +17,10 @@ provider "aws" {
   # Configuration options
   region = "us-east-1"
   alias  = "virginia"
+  access_key = var.access_key
+  secret_key = var.secret_key
   default_tags {
     tags = var.tags
-    # access_key = var.access_key
-    # secret_key = var.secret_key
   }
 }
 
